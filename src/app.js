0ab267a45361f4502d8551e3521e2944ecc3dacf
beanama <129529299+beanama@users.noreply.github.com>
2023-05-19 19:19:43 +0000

    feat:domain-name-generator created

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const selectedPronoun = pronoun[getRandomInt(pronoun.length)];
+  const selectedAdj = adj[getRandomInt(adj.length)];
+  const selectedNoun = noun[getRandomInt(noun.length)];
+
+  const domainName = `${selectedPronoun}${selectedAdj}${selectedNoun}.com`;
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  console.log(domainName);
 };
